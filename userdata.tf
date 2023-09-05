d68715a0ec738ffd09f68f719d2319368fdaf2bc
vgorthi <gorthivani2020@gmail.com>
2023-09-05 16:21:21 +0530

    parameter

--- userdata.tf
+++ userdata.tf
@@ -2,4 +2,4 @@
 
 yum install ansible python3.11-pip.noarch -y &>>/opt/userdata.log
 pip3.11 install botocore boto3 &>>/opt/userdata.log
-ansible-pull -i localhost, -U https://github.com/raghudevopsb74/roboshop-ansible.git main.yml -e component=${component} -e env=${env} &>>/opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/Gorthivani/workstation-ansible.git main.yml -e component=${component} -e env=${env} &>>/opt/userdata.log
